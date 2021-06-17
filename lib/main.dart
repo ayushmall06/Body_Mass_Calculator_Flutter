61c9192a92d208425c0618be86ec880870ce67a8
Ayush Mall <ayushmall@Ayushs-MacBook-Pro.local>
2021-06-17 14:24:51 +0530

    Day_Start

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+      theme: ThemeData(
+        primaryColor: Color(0x0FF0A0E21),
+        accentColor: Colors.blueGrey,
+      ),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){},
+      ),
+    );
+  }
+}
+
+
+
