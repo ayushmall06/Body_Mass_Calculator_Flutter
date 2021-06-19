6a9bff341f2769f3c4b85537678d41c85034548d
Ayush Mall <ayushmall@Ayushs-MacBook-Pro.local>
2021-06-19 22:11:11 +0530

    Day 3 Completed

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'card_content.dart';
+import 'reuseable_card.dart';
 
-
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerHeight = 80.0;
+final bottomContainerColor = Colors.pink.shade600;
 
 
 class InputPage extends StatefulWidget {
@@ -22,50 +27,66 @@ class _InputPageState extends State<InputPage> {
       body: Column(
         children: [
           Expanded(
-            child: Expanded(
-              child: Row(
-                children: [
-                  Expanded(child: ReUseableCard(colour: Color(0xFF1D1E33))),
-                  Expanded(child: ReUseableCard(colour: Color(0xFF1D1E33))),
-                ],
-              ),
+            child: Row(
+              children: [
+                Expanded(child:
+                ReUseableWidget(
+                  colour :activeCardColor,
+                  cardChild: CardContent(
+                    icon: FontAwesomeIcons.mars,
+                    text: 'MALE',
+                  ),
+                )),
+                Expanded(child:
+                    ReUseableWidget(
+                      colour : activeCardColor,
+                      cardChild: CardContent(
+                        icon: FontAwesomeIcons.venus,
+                        text: 'FEMALE',
+
+                      ),
+                    )
+                 ),
+              ],
             ),
 
           ),
-          Expanded(child: ReUseableCard(colour: Color(0xFF1D1E33)),),
+          Expanded(child:
+              ReUseableWidget(
+                colour : Color(0xFF1D1E33),
+                cardChild: Container(),
+              )
+                       ),
           Expanded(
-            child: Expanded(
-              child: Row(
-                children: [
-                  Expanded(child: ReUseableCard(colour: Color(0xFF1D1E33))),
-                  Expanded(child: ReUseableCard(colour: Color(0xFF1D1E33))),
-                ],
-              ),
+            child: Row(
+              children: [
+                Expanded(child:
+                    ReUseableWidget(
+                      colour : Color(0xFF1D1E33),
+                      cardChild: Container(),
+                    )
+                          ),
+                Expanded(child:
+                ReUseableWidget(
+                  colour : Color(0xFF1D1E33),
+                  cardChild: Container(),
+                ))
+              ],
             ),
-
           ),
+          Container(
+            height: bottomContainerHeight,
+            width: double.infinity,
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top:15),
+          )
 
-        ],
-      ),
+        ],)
 
     );
   }
 }
 
-class ReUseableCard extends StatelessWidget {
-  ReUseableCard({required this.colour});
-  late Color colour;
 
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-    );
-  }
-}
 
